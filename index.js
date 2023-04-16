14fbcc874f55cd5aa682eb4722d8ab2bc107b353
Luciano Germani <lucianogermani93@gmail.com>
2023-04-16 20:51:44 +0200

    Fix: Cors

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
-//Requiero express
-const express = require('express')
+const app                           = express()
+const express                       = require('express')
+const db                            = require('./config/db')
+const cors                          = require('cors');
+const corsOptions                   = require("./libs/cors");
+const port                          = process.env.PORT || 4000;
 
-//Utilizo dotenv para esconder las variables de entorno
-require('dotenv').config()
 
-//Requiero mi db en index.js
-const db = require('./config/db')
+const reviewRoutes                  = require('./routes/review.routes');
 
-//Hago que la constante App sea igual al metodo express()
-const app = express()
-
-//Obligo a que app cada vez que se ejecute pase por el middleware express.json que lo que hace es analizar la solicitud JSON entrante y la devuelve analizando los datos en req.body
+require('dotenv').config();
 app.use(express.json())  
-
-
-//Connect Routes
-const reviewRoutes = require('./routes/review.routes');
-//Routes
+app.use(cors(corsOptions));
 app.use('/api',reviewRoutes);
 
 
 
-//Cors
-const cors = require('cors');
-const corsOptions = 
-{
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
-    optionsSuccessStatus: 204,
-};
-//Request to app to use cors
-app.use(cors(corsOptions));
-
-//Seteo el puerto utilizando la variable de entorno PORT o si no la encuentra la 4000
-const port = process.env.PORT || 4000;
-
-
-//Aqui dependiendo del resultado anterior me devuelve en consola si esta conectado y a que puerto o me da un error
 db().then(() => {
     app.listen(port, () => {
         console.log('Server is running: ' + port);
